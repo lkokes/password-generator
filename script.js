dfe030ee88a58b6df795cd2cc0c5b05fd304768f
Lucia Kokesova <lkokesova@gmail.com>
2021-10-02 21:34:28 +0200

    Set characters

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// ABCDEFGHIJKLMNOPQRSTUVWXWZ
-// abcdefghijklmnopqrstuvwxwz
-// !\"$%&/()=?@~`\\.\';:+=^*_-"
-// 0123456789
+const upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXWZ';
+const lowerChar = 'abcdefghijklmnopqrstuvwxwz';
+const symbols = '!\"$%&/()=?@~`\\.\';:+=^*_-"';
+const numbers = '0123456789';
 
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXWZabcdefghijklmnopqrstuvwxwz!\"$%&/()=?@~`\\.\';:+=^*_-"0123456789';
 let output = document.getElementById("output");
 
 function randomValue(value){
@@ -14,11 +13,26 @@ function genPassword(){
     let length = document.getElementById('length').value;
     document.getElementById("length-val").textContent = length;
     let str = '';
+    let finalString = '';
+
+    if (document.getElementById("numbers").checked){
+        finalString += numbers
+    } 
+    if (document.getElementById("lowercase-char").checked){
+        finalString += lowerChar
+    }
+    if(document.getElementById("uppercase-char").checked){
+        finalString += upperChar
+    }
+    if(document.getElementById("symbols").checked){
+        finalString += symbols
+    }
 
     for(let i=0; i<length; i++){
-        let random = randomValue(characters.length);
-        str += characters.charAt(random);
+        let random = randomValue(finalString.length);
+        str += finalString.charAt(random);
     }
+
     output.value = str;
 }
 
